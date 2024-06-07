b46cbc71e384980d8678d2c643a7adbd8aaf379e
lampforamp <137897062+lampforamp@users.noreply.github.com>
2024-06-07 15:15:51 +0300

    123

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+export default {
+    root: './',
+    publicDir: '../static/',
+    base: '/WebGl_Viewer_Template/',
+    server:
+    {
+        host: true, // Open to local network and display URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: '../dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true // Add sourcemap
+    },
+}
